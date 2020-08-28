d0291fe8c15d1beac24d1fa181a21e9fc9d07110
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-08-29 00:12:00 +0200

    terminata bozza

--- js/script.js
+++ js/script.js
@@ -13,61 +13,89 @@
 //
 
 // dichiaro variabili
-var mine = 6;
-var attempts = 100;
+// numero di mine
+var mine = 16;
+// range di numeri
+var numRange = 100;
+// lista con la posizione delle mine
 var minePosition = [];
+// lista dei numeri inseriti
 var userPosition = [];
+// vincitori ottimisti
+var vinto = true;
 
-// funzione che dato array e numero mi controlla se e' stato gia' inserito
+// funzione che controlla un valore con gli elementi di un array
 function isDuplicate(numList, numInserted) {
-  for (var i = 0; i < numList.length; i++) {
-    if (numList[i] == numInserted){
-      var positionOn = true;
+  var i = 0;
+  var duplicateOn = false;
+  while (i < numList.length && duplicateOn == false) {
+    if (numList[i] == numInserted) {
+      duplicateOn = true;
     }
+    i++
   }
-  return positionOn;
+  return duplicateOn;
 }
 
 
 // creo un ciclo che mi genera 16 numeri
 for (var i = 0; i < mine; i++) {
   do {
-   var numCpu = Math.floor(Math.random() * attempts) + 1;
+   var numCpu = Math.floor(Math.random() * numRange) + 1;
  } while (isDuplicate(minePosition, numCpu));
+
   minePosition[i] = numCpu;
 }
-console.log(minePosition);
+console.log("numeri posizione mine " + minePosition);
 
 
-// chiedo all'utente di inseerire tot numeri
+// chiedo all'utente un numero da 1 a 100
+// controllo che sia da 0 a 100 e che non sia gia' stato scritto nell'array
+
+console.log("numeri da 1 a " + numRange);
+console.log("Tot livelli: " + (numRange - mine));
+
+// inserisco un ciclo while per richiederlo fino a numRange (100-16)
 var i = 0;
-var isMine = false;
-while (i < (attempts - mine) && isMine == false) {
-  var numUser = parseInt(prompt("inserisci un numero"));
+var finito = false;
+while (i < (numRange - mine) && finito == false) {
+  console.log("livello: " + (i+1));
 
-   var isDuplicated = isDuplicate(userPosition, numUser);
-  console.log(isDuplicated);
-  while (isDuplicated == true) {
-    numUser = parseInt(prompt("sbagliato reinserisci un numero"));
-    isDuplicated = isDuplicate(userPosition, numUser);
-  }
-
-
-  isMine = isDuplicate(minePosition, numUser);
-  if (isMine) {
-    alert("mi spiace e' esplosa la mina!");
-  }else {
-    userPosition[i] = numUser;
-
-    if (i == (attempts - mine) - 1){
-      alert("complimenti hai vinto! Sei arrivato al " + i + " livello");
-      alert("Hai vinto con i seguenti numeri " + userPosition)
-      isMine = true;
+  // controllo che il numero non sia gia' stato inserito o che non e' all'interno nel numRange
+  var numValidate = false;
+  do {
+    var numUtente = parseInt(prompt("inserisci un numero da 1 a " + numRange));
+    if (isDuplicate(userPosition, numUtente) == true) {
+      alert("Attenzione il numero e' gia' stato inserito");
+      numValidate = false;
+    }else if (numUtente > numRange) {
+      alert("Attenzione il numero inserito non e' nel range indicato");
+      numValidate = false;
+    }else {
+      numValidate = true;
     }
+  } while (numValidate == false);
 
+  // se il numero e' stato validato allora controllo se e' uguale alla posizione delle mine
+  console.log("numero inserito: " + numUtente)
+  var isMine = isDuplicate(minePosition, numUtente);
+  console.log("era una mina? " + isMine);
+  // se isMine e' true finira' il ciclo while e la variabile vinto cambiata in false (hai perso)
+  if (isMine) {
+    finito = true;
+    vinto = false;
+  // altrimenti aggiungera' la posizione all'interno dell'array userPosition e poi incrementerà di 1 la var i
+  } else {
+    userPosition[i] = numUtente;
+    console.log(userPosition);
+    i++
   }
-  console.log(userPosition);
-  console.log(i);
-  i++;
-  console.log(i);
+
+}
+
+
+if (vinto == true) {
+  alert("hai vinto!");
+} else {
+  alert("mi dispiace e' esplosa la mina! Hai perso.")
 }
